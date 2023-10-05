df60262afc06d42cf6b15e78e5370a7c15fc5c89
Jotape <juanpa1999a@gmail.com>
2023-10-04 23:25:38 -0300

    Add Axios and Fetch function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
+//Navbar section
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
+//Views
 import Favorites from "./views/Favorites";
 import Home from "./views/Home";
-
+//Fetch section
+import axios from 'axios';
+import { useEffect } from 'react';
 const PHOTO_URL = "/photos.json";
 
 const App = () => {
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+  
+  const fetchPhotos = async () => {
+    axios
+      .get(PHOTO_URL)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((err) => {
+        console.log("Error al obtener datos de la API:", err);
+      });
+  };
+
   return (
     <div>
       <Navbar />
