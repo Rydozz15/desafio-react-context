c875fcde7afe32b94683dec8abab892497dc854a
Jotape <juanpa1999a@gmail.com>
2023-10-07 00:25:50 -0300

    Add Context enviroment to Favorites

--- src/views/Favorites.jsx
+++ src/views/Favorites.jsx
@@ -1,8 +1,15 @@
+import { useContext } from "react";
+import { GalleryContext } from "../context/GalleryContext";
 const Favorites = () => {
+
+  const {gallery, setGallery} = useContext(GalleryContext)
+
   return (
     <div>
       <h1>Fotos favoritas</h1>
-      <div className="p-3 gallery grid-columns-4"></div>
+      <div className="p-3 gallery grid-columns-4">
+      
+      </div>
     </div>
   );
 };
