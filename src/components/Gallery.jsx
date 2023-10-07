8e40a0f783ecf8b5ef36c8afa7ece69958c3b5a0
Jotape <juanpa1999a@gmail.com>
2023-10-06 23:03:13 -0300

    Add Context consumer and Gallery render

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,19 @@
+import { useContext } from "react";
+import { GalleryContext } from "../context/GalleryContext";
+import IconHeart from "./IconHeart";
 const Gallery = () => {
-  return <div className="gallery grid-columns-5 p-3"></div>;
+  const {gallery, setGallery} = useContext(GalleryContext)
+  return <div className="gallery grid-columns-5 p-3">
+    {gallery == []
+          ? '' 
+          : 
+          gallery.map(el =>
+            <div className="photo" key={el.id} style={{backgroundImage: `url(${el.src.original})`}}>
+              {el.src.liked ?  <IconHeart filled={true} /> : <IconHeart filled={false}/>}
+              {el.alt}
+            </div>
+          )
+        }
+  </div>;
 };
 export default Gallery;
