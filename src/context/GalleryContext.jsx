a86671c01fd0c367164284b645561e99d3742f19
Jotape <juanpa1999a@gmail.com>
2023-10-06 23:02:54 -0300

    Add API Fetch with Axios, moved from App

--- src/context/GalleryContext.jsx
+++ src/context/GalleryContext.jsx
@@ -1,9 +1,30 @@
 import { createContext, useState } from "react";
+//Fetch section
+import axios from 'axios';
+import { useEffect } from 'react';
+const PHOTO_URL = "/photos.json";
 
 export const GalleryContext = createContext();
 
 const GalleryProvider = ({ children }) => {
     const [gallery, setGallery] = useState([]);
+    
+    //Fetch gallery
+    useEffect(() => {
+        fetchPhotos();
+      }, []);
+            
+      const fetchPhotos = async () => {
+        axios
+          .get(PHOTO_URL)
+          .then((response) => {
+            setGallery(response.data.photos)
+          })
+          .catch((err) => {
+            console.log("Error al obtener datos de la API:", err);
+          });
+      };
+    
     return (
         <GalleryContext.Provider value={{ gallery, setGallery }}>
             {children}
